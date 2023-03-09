a0e701ab7c65943c7f1f5507709c9ee02adee6eb
akhmadazizmario <akhmadazizmario10@gmail.com>
2023-03-09 15:58:47 +0700

    terbaru

--- resources/views/tutorkomen.blade.php
+++ resources/views/tutorkomen.blade.php
@@ -10,9 +10,9 @@
             <img src="https://blogger.googleusercontent.com/img/a/AVvXsEj4zmdFN7quSLd_SfAhqhJHNcal5N2f1eeslysDXOGF5PmVP9eP0T8UtHYlPFa7RZe71fr7ecoUdKr9ub7eyaep7ZdFi1RVTfmzy5g4R9S6-JUHO5TviWN_q866tYwakgrjTf53xPSOQ8E6hSr6JJGvb8zW568wqUF7_LqFmIFK-aXVtIfovbhgRSQiTg"
                 alt="">
             <br>
-            <h4><strong>2.</strong> Jika belum punya akun klik <font color="blue">butuh akun?</font>.
+            <h4><strong>2.</strong> Jika belum punya akun klik <font color="blue">butuh akun?</font>
                 <h4><strong>3.</strong> Jika sudah punya
-                    akun masuk dengan email dan password yang sudah anda daftar di disqus.</h4><br><br>
+                    akun masuk dengan email dan password yang sudah anda daftar di disqus</h4><br><br>
                 <center><img
                         src="https://blogger.googleusercontent.com/img/a/AVvXsEjrlriFKyEMBh52E0HAyfoSQT9xCON_VZ4SW93UNmbYgjE9meNBWKdcdWjcAacXPcWPhcyHZKlSVTWM1maejxngJ_haUBKUiGlukx8KoVmZlYpFWeD6MkFlxeWDI2o_V1K3ed5OMaFeAwuSTokzHPl_XkwvRv18ApG-M20XVRONAJZy5QLZ31rf2Aw1tw"
                         alt="" height="400px" width="50%"></center>
