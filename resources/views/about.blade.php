42d12e322359d3cc6fe71d0e1a7e672f25b6c05f
akhmadazizmario <akhmadazizmario10@gmail.com>
2023-03-28 12:44:53 +0700

    terbaru

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,22 +6,29 @@
         <div class="container">
             <h1>Halaman Tentang Kami</h1>
             <hr>
-            <h3>{{ $name }}</h3><br>
-            <img src="/img/Untitled.png" alt="aziz" width="200"><br>
-            <p> Aplikasi Web BeritaGenZ dibuat bertujuan memudahkan
-                semua orang untuk bereksplor dan membuat <br>
-                berita yang menarik minat buat pembaca. dan saya membuat website ini bersifat formal atau semuanya bisa <br>
-                membuat akun dan membuat postingan di website BeritaGenZ ini.<br>
-                <br>
-                <br>
-                <strong>Kata Motivasi :</strong>
-            <p> Carilah ilmu sebanyak mungkin dan harus tetap semangat,<br>
-                Karena kesuksesan itu pasti berawal dari kata semangat.<br>
-                <strong>By.AzizMario</strong>
-            </p>
-            <strong>Take care dan selalu jaga kesehatan <i class="bi bi-emoji-wink"></i>.</strong>
-
-            </p>
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>{{ $name }}</h3><br>
+                    <img src="/img/Untitled.png" alt="aziz" width="200"><br>
+                    <p> Aplikasi Web BeritaGenZ dibuat bertujuan memudahkan
+                        semua orang untuk bereksplor dan membuat <br>
+                        berita yang menarik minat buat pembaca. dan saya membuat website ini bersifat formal atau semuanya
+                        bisa membuat akun dan membuat postingan di website BeritaGenZ ini.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <strong>Kata Motivasi :</strong>
+                    <p> Carilah ilmu sebanyak mungkin dan harus tetap semangat,<br>
+                        Karena kesuksesan itu pasti berawal dari kata semangat.<br>
+                        <strong>By. Admin BeritaGenZ</strong>
+                    </p>
+                    <strong>Take care dan selalu jaga kesehatan <i class="bi bi-emoji-wink"></i>.</strong>
+                    <br><br><br><br><br>
+                    <p style="color:black">More Questions? Email us at
+                        <strong>admission@beritagenz.com</strong>
+                    </p>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
